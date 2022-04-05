c4f22f7dd6172aa5484b63ae97bf223331d74c6a
Gracious Nguwo <73261241+Gracious265@users.noreply.github.com>
2022-04-05 19:33:17 +0000

    getting user input

--- main.dart
+++ main.dart
@@ -1,11 +1,18 @@
 import "dart:math";
+import "dart:io";
 
 void main() {
+
+  // Getting user input
   
-  String greeting = "Hello";
+  print("What is your name?:");
+  String username = stdin.readLineSync()!;
+  print("Hello ${username}");
+  
+  //String greeting = "Hello";
   // learning more about strings.
   
-  String anotherGreeting = " World";
+  //String anotherGreeting = " World";
   
   //print(greeting.length);
   //print(greeting[4]);
@@ -13,7 +20,7 @@ void main() {
   //print(greeting.indexOf("e")); // the indexOf
   //print(greeting.indexOf("o"));
   //print(greeting.contains("e")); // returns a boolean
-  //print(greeting.contains("8")); // this returns a false boolean
+  //print(greeting.contains("8")); // this returns a false   boolean
   //print(greeting + anotherGreeting);
 
   //print("The passoword is:  ${greeting}");
@@ -22,23 +29,29 @@ void main() {
 
   // working with numbers
 
-  int quantity = 300;
-  double price = 5.99;
+  //int quantity = 300;
+  //double price = 5.99;
 
   //quantity = quantity + 50;
   //quantity += 50; // same as quantity = quantity + 50
   
   //quantity += 1;
 
-  quantity++;
+  //quantity++;
   
   
  // print("The total price is: ${quantity}");
 
   //mathematical operations in dart using imported module
   
-  print(min(1, 9)); // gets the minmum number
-  print(max(100,8)); // gets the maxmum number
-  print(sqrt(144)); // gets the square root
+  //print(min(1, 9)); // gets the minmum number
+  //print(max(100,8)); // gets the maxmum number
+  //print(sqrt(144)); // gets the square root
+
+  //booleans expression
+  //print(true);
+  //print(5 > 3);
+  //print(5 == 1);
+  //print(1 >= 8);
 
 }
